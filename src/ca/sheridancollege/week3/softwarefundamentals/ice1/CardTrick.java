604678aaf9cae5dc297357b6040eeacc3613df91
Breshna-Rahimzy-Sheridan <rahimzy@sheridancollege.ca>
2019-02-05 18:25:20 -0500

    Complete the CardTrick class so that it models a “hand” of seven cards as an array

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -20,6 +20,7 @@ public class CardTrick {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         Card[] magicHand = new Card[7];
+        
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
