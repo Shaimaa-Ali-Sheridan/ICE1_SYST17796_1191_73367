297a58313841f4298f5c8378af510ce5bc0288ba
Owner <Owner@10.48.48.206>
2019-02-05 18:05:05 -0500

    project

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,16 +10,22 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @author Ruhi ruhi Student number: 991528631
+ * feb/o5/2019
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+          int hand = (int)(Math.random())*7;
+          
         
         for (int i=0; i<magicHand.length; i++)
         {
+          
             Card c = new Card();
+            System.out.println("pick the cards");
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
