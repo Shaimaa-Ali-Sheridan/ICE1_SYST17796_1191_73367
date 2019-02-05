104c457daf5d3bf1df0228fefbbba7337f3c9bd5
donalddt-Sheridan <donalddt@sheridancollege.ca>
2019-02-05 17:38:58 -0500

    Merge commit '84efff4e0de3b20f1ac572539f8aff7eaefb452a'
    Conflicts:
    	src/studentslist/Student.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,22 +4,30 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @Author Tim Donald-Davis
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        
+        Scanner k = new Scanner(System.in);
         Card[] magicHand = new Card[7];
+        
+      
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
+            c.setValue((int)(1+ Math.random()*12));
+            c.setSuit(Card.SUITS[(int)(1 + Math.random()* 2)]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
@@ -27,7 +35,20 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        int userCard = 0;
+        String UserSuit = "";
+        System.out.println("Pick a card value: ");
+          userCard = k.nextInt();
+          
+          System.out.println("Pick a suit: ");
+          UserSuit = k.nextLine().trim();
+          
+          Arrays.sort(magicHand);
+          
+          
+          
     }
     
     
 }
+
