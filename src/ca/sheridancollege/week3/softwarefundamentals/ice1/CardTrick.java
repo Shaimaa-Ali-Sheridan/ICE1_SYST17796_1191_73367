4bea0d0a625550aba9b91ce25b56efeb120a7942
Harman-Bath <bathharm@sheridancollege.ca>
2019-02-05 17:56:30 -0500

    Completed steps 1-4 for ICE 1.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,23 +10,51 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Harman Bath
+ * 991540198
  */
+import java.util.Scanner; 
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
+        for (int sub=0; sub<magicHand.length; sub++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int randomNum = 1+(int)(Math.random()*13); 
+            c.setValue(randomNum);
+            int randomSuit = (int)(Math.random()*3 + 1); 
+            c.setSuit(Card.SUITS[randomSuit]); 
+            magicHand[sub] = c; 
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        Scanner k = new Scanner(System.in); 
+        System.out.println("Pick any suit (Hearts, Diamonds, Clubs, Spades): ");
+        String suit = k.next(); 
+        System.out.println("Pick the card number (1-13): ");
+        int value = k.nextInt(); 
+        
+        boolean found = false; 
+        for(int sub = 0; sub < magicHand.length; sub++){
+            if(magicHand[sub].getValue() == value && magicHand[sub].getSuit() == suit){
+                found = true; 
+            }
+        }
+        
+        if(found == true)
+            System.out.println("Card Found");
+        else
+            System.out.println("Card not found"); 
+                
+        
     }
     
     
