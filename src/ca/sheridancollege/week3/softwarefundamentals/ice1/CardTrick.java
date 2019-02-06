a6689e2ffc2d349127bba73e3468c68627c4c9ae
Anmol-Nagpal <= nagpaanm@sheridancollege.ca>
2019-02-06 01:21:41 -0500

    Completed step 7. Fixed suits random num range (now truly 0-3, unlike 1-3 previously).

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -14,8 +14,6 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * 991154162
  */
 
-import java.util.Scanner;
-
 public class CardTrick {
     
     public static void main(String[] args)
@@ -25,28 +23,20 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        
             int randomNum =  1 + (int)((Math.random()) * 13);
             c.setValue(randomNum);
-            int randomSuit = (int)((Math.random()) * 3 + 1);
+            int randomSuit = (int)((Math.random()) * 4);
             c.setSuit(Card.SUITS[randomSuit]);
             magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
         Card luckyCard = new Card();
         luckyCard.setSuit("Hearts");
         luckyCard.setValue(5);
-        
-        Scanner k = new Scanner(System.in);
-        System.out.println("Pick the suit (Hearts, Diamonds, Spades, Clubs): ");
-        String suit = k.next();
-        System.out.println("Pick the value (1-13)");
-        int val = k.nextInt();
-        
+        int val = luckyCard.getValue();
+        String suit = luckyCard.getSuit();
+ 
         boolean found = false;
         for(int i=0; i < magicHand.length; i++){
             if(magicHand[i].getValue() == val && 
@@ -55,9 +45,9 @@ public class CardTrick {
             }
         }
         if(found == true){
-            System.out.println("Card found");
+            System.out.println("Card found. You Win!");
         }else{
-            System.out.println("Card not found");
+            System.out.println("Card not found. You Lose");
         }      
     }
         
