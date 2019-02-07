c4f3327d1805c49e450970246a66130551c1195a
tatelg <tatelg@sheridancollege.ca>
2019-02-07 17:52:55 -0500

    -fixed Math.random- -added name and student I.D

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  * @author Gregory Tatel
- * Student I.D:
+ * Student I.D: 991545515
  */
 public class CardTrick {
     
