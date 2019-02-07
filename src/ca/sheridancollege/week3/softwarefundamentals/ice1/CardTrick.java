e77f043cfb54354e5ac701ba34fda4fab550b4a2
Jeswin <46729812+Jeswin-K-Jessor@users.noreply.github.com>
2019-02-07 11:38:01 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -17,6 +17,7 @@ import java.util.Scanner;
  */
 
 public class CardTrick {
+   
     
     public static void main(String[] args)
     {
@@ -40,6 +41,7 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        Card luckyCard = new Card();
         
        
        
